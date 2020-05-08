3d8069326815d499bc6edaeb84bee23601a36f38
marlonjf <marlon.jf12@gmail.com>
2020-05-07 23:35:16 -0300

    Updates

---
+++ precache-manifest.b1790a601e82c1acaa21a89abdeb8881.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c63a77a5668e4542a6335ed529107e01",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "c0f85151ab2366a6b6bc",
+    "url": "/react-project/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "bff5aa5231e2345679eb",
+    "url": "/react-project/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c0f85151ab2366a6b6bc",
+    "url": "/react-project/static/js/2.368c3856.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-project/static/js/2.368c3856.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bff5aa5231e2345679eb",
+    "url": "/react-project/static/js/main.15248be2.chunk.js"
+  },
+  {
+    "revision": "080408a5389ce51f0fdd",
+    "url": "/react-project/static/js/runtime-main.4eafe2c7.js"
+  }
+]);
